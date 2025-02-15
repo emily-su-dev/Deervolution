3a70ecd67ac80c64f299dcbcc2a154b4eedcf7f8
Daniyal Bokhari <daniyalcraft123@gmail.com>
2025-02-15 10:02:54 -0500

    move folders

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home.tsx";
-import Activity from "./pages/Activity.tsx";
-import Picture from "./pages/Picture";
+import Home from "./pages/Home/Home.tsx";
+import Activity from "./pages/Activity/Activity.tsx";
+import Picture from "./pages/Picture/Picture.tsx";
 
 function App() {
   return (
