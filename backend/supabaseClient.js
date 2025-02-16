def181dfe35168aa07517f550055901e31017c5c
Daniyal Bokhari <daniyalcraft123@gmail.com>
2025-02-15 19:40:29 -0500

    fix

--- backend/supabaseClient.js
+++ backend/supabaseClient.js
@@ -1,4 +1,5 @@
 const { createClient } = require('@supabase/supabase-js');
+require('dotenv').config();
 
 const supabaseUrl = process.env.VITE_SUPABASE_URL;
 const supabaseKey = process.env.VITE_SUPABASE_KEY;
