d61a4747335210d8ffa9c7cb0b0aa17e07c41fe1
Daniyal Bokhari <daniyalcraft123@gmail.com>
2025-02-16 00:15:30 -0500

    clean App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,3 @@
-// import { useEffect, useState } from 'react';
 import './App.css';
 import SignIn from './pages/SignIn/SignIn';
 import SignUp from './pages/SignUp/SignUp';
@@ -9,39 +8,7 @@ import Picture from "./pages/Picture/Picture";
 import { AuthProvider } from './context/AuthContext';
 import { ProtectedRoute } from './components/ProtectedRoute';
 
-// // Define the interface for your data
-// interface AccountData {
-//   userid: string;
-//   deers: number;
-//   geese: number;
-//   racoons: number;
-//   squirrels: number;
-//   sparrow: number;
-// }
-// const VITE_BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:8000";
-
 function App() {
-  // const [data, setData] = useState<AccountData[]>([]);
-  // const [loading, setLoading] = useState(true);
-
-  // useEffect(() => {
-  //   const getData = async () => {
-  //     try {
-  //       const response = await fetch(`${VITE_BACKEND_URL}/data`);
-  //       const fetchedData = await response.json();
-  //       setData(fetchedData);
-  //       console.log('Fetched data: ', fetchedData);
-  //     } catch (error) {
-  //       console.error('Error loading data:', error);
-  //     } finally {
-  //       setLoading(false);
-  //     }
-  //   };
-
-  //   getData();
-  // }, []);
-
-  // if (loading) return <p>Loading...</p>
   return (
     <AuthProvider>
       <Router>
