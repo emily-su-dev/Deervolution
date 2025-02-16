e75b46cb864c6f92802bb06468d8c3e0764b8c45
Warrick Tsui <warricktsui1226@gmail.com>
2025-02-15 19:42:09 -0500

    bruh

--- backend/supabaseClient.js
+++ backend/supabaseClient.js
@@ -1,8 +1,8 @@
 const { createClient } = require('@supabase/supabase-js');
 require('dotenv').config();
 
-const supabaseUrl = process.env.VITE_SUPABASE_URL;
-const supabaseKey = process.env.VITE_SUPABASE_KEY;
+const supabaseUrl ="https://jtoizyannctdzsgkepmy.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp0b2l6eWFubmN0ZHpzZ2tlcG15Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk2MDEzMzcsImV4cCI6MjA1NTE3NzMzN30.tXCwQKmqypE3WU0xjnlj3_BehsXSDLbxbo6yu3qillo";
 
 // Initialize Supabase client
 const supabase = createClient(supabaseUrl, supabaseKey);
