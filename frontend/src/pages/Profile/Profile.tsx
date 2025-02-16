fbdf405c06b5f98ee9c4585fee91e8b2f08a5df7
Emily Su <emilysucanada@gmail.com>
2025-02-16 07:48:09 -0500

    fix profile page styling, leaderboard page styling

--- frontend/src/pages/Profile/Profile.tsx
+++ frontend/src/pages/Profile/Profile.tsx
@@ -5,6 +5,9 @@ import { useAuth } from "../../context/AuthContext"; // Import authentication co
 import "./Profile.css"; // Import CSS styles
 const VITE_BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:8000";
 
+import logo from "../../assets/deervolution_logo.png";
+import upper from "../../assets/deerv_upper_decor.png";
+
 const Profile: React.FC = () => {
     const navigate = useNavigate();
     const { user } = useAuth(); // Get the logged-in user
@@ -43,6 +46,10 @@ const Profile: React.FC = () => {
             <button className="back-button" onClick={() => navigate("/activity")}>
                 🔙 Back
             </button>
+            
+            {/* Upper Decorative Shape */}
+            <img src={upper} alt="Upper Decor" className="upper-decor" />
+            <img src={logo} alt="Deervolution Logo" className="logo" />
 
             {/* Profile Title */}
             <div className="title-container">
@@ -50,7 +57,7 @@ const Profile: React.FC = () => {
             </div>
 
             {/* Stats Section - Hardcoded Instead of Looping */}
-            <div className="stats-container">
+            <div className="stats-container-lvl1">
                 <div className="stat-item">
                     <img src="/icons/deer.png" alt="Deer" className="animal-icon" />
                     <span className="animal-count">{stats.deer}</span>
@@ -63,6 +70,9 @@ const Profile: React.FC = () => {
                     <img src="/icons/raccoon.png" alt="Raccoon" className="animal-icon" />
                     <span className="animal-count">{stats.raccoon}</span>
                 </div>
+            </div>
+
+            <div className="stats-container-lvl2">
                 <div className="stat-item">
                     <img src="/icons/squirrel.png" alt="Squirrel" className="animal-icon" />
                     <span className="animal-count">{stats.squirrel}</span>
